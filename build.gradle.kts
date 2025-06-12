9d6d3e47780fa2097858088e11add5963e4f8fa4
droidbaza <iigospodarik@rusatom-utilities.ru>
2025-06-12 14:53:05 +0300

    publish configs

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,19 @@ plugins {
 group = "com.github.droidbaza"
 version = "1.0.0-alpha01"
 
+gradlePlugin {
+    website = "https://github.com/droidbaza/logmeplugin"
+    vcsUrl =  "https://github.com/droidbaza/logmeplugin.git"
+    plugins {
+        create("logMePlugin") {
+            id = "com.github.droidbaza.logmeplugin"
+            displayName = "LogMe Plugin"
+            description = "Easy logging everything!"
+            implementationClass = "LogMePlugin"
+        }
+    }
+}
+
 repositories {
     mavenCentral()
 }
