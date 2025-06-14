426e16dd18a634a5b4f923face4fdd4b8bad2aa0
droidbaza <iigospodarik@rusatom-utilities.ru>
2025-06-14 14:53:29 +0300

    fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,13 @@
 plugins {
     kotlin("jvm") version "2.1.10"
-    id("com.gradle.plugin-publish") version "1.3.0"
 }
 
 group = "org.droidbaza"
 version = "1.0.0-alpha02"
 
-gradlePlugin {
-    website = "https://github.com/droidbaza/logmeplugin"
-    vcsUrl =  "https://github.com/droidbaza/logmeplugin.git"
-    plugins {
-        create("logMePlugin") {
-            id = "org.droidbaza.logme"
-            displayName = "LogMe Plugin"
-            tags = listOf("log","logger")
-            description = "Easy logging everything!"
-            implementationClass = "com.github.droidbaza.logmeplugin.LogMePlugin"
-        }
-    }
-}
-
-repositories {
+repositories{
     mavenCentral()
 }
-
 dependencies {
     implementation(kotlin("compiler-embeddable"))
 }
